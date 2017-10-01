ce2896ddb3d6aa0467bbb917fe8447af5cdaa596
danlurie <danjlurie@gmail.com>
2017-10-01 13:03:41 -0700

    Push from local

--- code/fmriprep/sge/submit.sh
+++ code/fmriprep/sge/submit.sh
@@ -1,4 +1,4 @@
 submit \
     -s /home/despoB/DistractWM/code/fmriprep/sge/sge_run.sh \
-    -f /home/despoB/DistractWM/code/fmriprep/sge/test_list.txt \
+    -f /home/despoB/DistractWM/code/fmriprep/sge/subject_list.txt \
     -o /home/despoB/DistractWM/code/fmriprep/sge/qsub.options
